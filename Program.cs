e14c3616e49dfd8422d8aec0eee1a9f5acaf48d2
Dayne Game <daynegame@Daynes-MacBook-Pro.local>
2017-07-24 11:32:36 +1200

    Hello Worlds

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Week03Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
